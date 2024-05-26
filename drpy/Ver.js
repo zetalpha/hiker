5e80363858fe48e5913d55f1cd60d34adb8540cb
zetalpha <ouzhiheng9437@gmail.com>
2024-05-26 18:39:45 +0800

    Update Ver.js

--- drpy/Ver.js
+++ drpy/Ver.js
@@ -2,7 +2,7 @@ var dict = {
     libs_hiker: {
         ver: "1.0.0",
         toast: '',
-        localPath: 'hiker://files/cache/drpy/Ver.js',
+        localPath: 'hiker://files/cache/drpy/libsVer.js',
         webPath: 'https://gitee.com/zetalpha/hikerview/raw/master/global/html/SlideX.html',
     }
 }
