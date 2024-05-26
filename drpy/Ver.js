62d6300b17b81b314b6766b929a0c9a3b23833c5
zetalpha <ouzhiheng9437@gmail.com>
2024-05-26 18:36:29 +0800

    Create Ver.js

---
+++ drpy/Ver.js
@@ -0,0 +1,8 @@
+var dict = {
+    libs_hiker: {
+        ver: "1.2.8",
+        toast: '设置增加 排序控制 增加长按链接控制',
+        localPath: 'hiker://files/cache/drpy/libs_hiker.ini',
+        webPath: 'https://gitee.com/zetalpha/hikerview/raw/master/global/html/SlideX.html',
+    }
+}
